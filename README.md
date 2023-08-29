48580c4d6cddf7cac554d9542e4f92cc32d3634f
Abdallah Mohamed <abdallah.mohamed@kyocera-avx.com>
2023-08-29 15:37:24 -0400

    updated

---
+++ README.md
@@ -0,0 +1,10 @@
+## How to use this pdf2html tool to convert pdf to html
+
+To run the script and convert a PDF file to HTML, follow these steps:
+
+1. Open a terminal or command prompt.
+2. Navigate to the directory where the `pdf2html.py` script is located.
+3. Run the following command: `python pdf2html.py path/to/input.pdf path/to/output.html`
+    - Replace `path/to/input.pdf` with the file path of the PDF you want to convert.
+    - Replace `path/to/output.html` with the desired file path and name for the output HTML file.
+4. Wait for the script to finish running. The converted HTML file will be saved to the specified output file path.
--- pdf2html.py
+++ pdf2html.py
@@ -1,19 +1,30 @@
 import pdfplumber
 from bs4 import BeautifulSoup
+import sys
 
 def pdf_to_html(pdf_path, html_path):
     with pdfplumber.open(pdf_path) as pdf:
-        soup = BeautifulSoup('<html><body></body></html>', 'html.parser')
+        soup = BeautifulSoup('<html><head><style>.pdf-text { position: absolute; }</style></head><body></body></html>', 'html.parser')
         body = soup.body
 
-        for page in pdf.pages:
-            text = page.extract_text()
-            div = soup.new_tag("div", style="page-break-before: always;")
-            div.string = text
-            body.append(div)
+        for i, page in enumerate(pdf.pages):
+            page_div = soup.new_tag("div", style=f"position: relative; width: {page.width}pt; height: {page.height}pt;")
+            body.append(page_div)
+            
+            for char in page.extract_words():
+                left = char['x0']
+                top = char['top']
+                text = char['text']
+                
+                span = soup.new_tag("span", **{"class": "pdf-text", "style": f"left: {left}pt; top: {top}pt;"})
+                span.string = text
+                page_div.append(span)
 
     with open(html_path, "w", encoding="utf-8") as f:
         f.write(str(soup))
 
 if __name__ == "__main__":
-    pdf_to_html("sample.pdf", "output.html")
+    if len(sys.argv) != 3:
+        print("Usage: python script_name.py pdf_path html_path")
+        sys.exit(1)
+    pdf_to_html(sys.argv[1], sys.argv[2])
